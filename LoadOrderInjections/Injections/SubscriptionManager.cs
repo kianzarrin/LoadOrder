9fad318b975cb0dde3cfcda04801e69d1c25310c
kian.zarrin <kian.zarrin@gmail.com>
2021-03-03 17:50:34 +0200

    fix for local/UTC time polish

--- LoadOrderInjections/Injections/SubscriptionManager.cs
+++ LoadOrderInjections/Injections/SubscriptionManager.cs
@@ -1,10 +1,8 @@
 using ColossalFramework.PlatformServices;
-using ColossalFramework.Plugins;
 using KianCommons;
 using System;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,8 +11,7 @@ namespace LoadOrderInjections {
         void Awake() => Log.Debug("TestComponent.Awake() was called");
         void Start() => Log.Debug("TestComponent.Start() was called");
 
-        void OnGUI()
-        {
+        void OnGUI() {
             if (GUILayout.Button("Check Installed"))
                 SteamUtilities.EnsureAll();
             if (GUILayout.Button("Delete UnInstalled"))
@@ -30,8 +27,7 @@ namespace LoadOrderInjections {
         // Load the Arial font from the Unity Resources folder.
         static Font Arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
 
-        void Awake()
-        {
+        void Awake() {
             Log.Debug("Example.Awake() was called");
 
             var canvas = AddCanvas();
@@ -48,8 +44,7 @@ namespace LoadOrderInjections {
             //    SteamUtilities.OnQueryItemsClicked);
         }
 
-        static Canvas AddCanvas()
-        {
+        static Canvas AddCanvas() {
             // Create Canvas GameObject.
             GameObject canvasGO = new GameObject();
             canvasGO.name = "Canvas";
@@ -63,8 +58,7 @@ namespace LoadOrderInjections {
             return canvas;
         }
 
-        static Text AddText(GameObject parent, string label)
-        {
+        static Text AddText(GameObject parent, string label) {
             // Create the Text GameObject.
             GameObject textGO = new GameObject();
             textGO.transform.parent = parent.transform;
@@ -104,8 +98,7 @@ namespace LoadOrderInjections {
         //    return button;
         //}
 
-        void Update()
-        {
+        void Update() {
             // Press the space key to change the Text message.
             if (Input.GetKeyDown(KeyCode.Space)) {
                 if (textChanged != UpDown.Down) {
@@ -121,8 +114,7 @@ namespace LoadOrderInjections {
 
     public static class SubscriptionManager {
         /// <returns>true, to avoid loading intro</returns>
-        public static bool PostBootAction()
-        {
+        public static bool PostBootAction() {
             bool sman = Environment.GetCommandLineArgs().Any(_arg => _arg == "-sman");
             if (sman) {
                 new GameObject().AddComponent<Camera>();
@@ -132,16 +124,14 @@ namespace LoadOrderInjections {
             return sman;
         }
 
-        public static void EnsureSubs()
-        {
+        public static void EnsureSubs() {
             DirectoryInfo d = new DirectoryInfo("");
         }
     }
 
     public static class SteamUtilities {
         // steam manager is already initialized at this point.
-        public static void RegisterEvents()
-        {
+        public static void RegisterEvents() {
             Log.Debug(Environment.StackTrace);
             PlatformService.eventSteamControllerInit += OnInitSteamController;
 
@@ -153,8 +143,7 @@ namespace LoadOrderInjections {
             PlatformService.workshop.eventUGCRequestUGCDetailsCompleted += OnUGCRequestUGCDetailsCompleted;
         }
 
-        public static void OnInitSteamController()
-        {
+        public static void OnInitSteamController() {
             Log.Debug(Environment.StackTrace);
             bool sman = Environment.GetCommandLineArgs().Any(_arg => _arg == "-sman");
             if (sman)
@@ -163,51 +152,43 @@ namespace LoadOrderInjections {
                 EnsureIncludedOrExcluded(id);
         }
 
-        public static void OnRequestItemDetailsClicked()
-        {
+        public static void OnRequestItemDetailsClicked() {
             Log.Debug("RequestItemDetails pressed");
             foreach (var id in PlatformService.workshop.GetSubscribedItems())
                 PlatformService.workshop.RequestItemDetails(id).LogRet($"RequestItemDetails({id.AsUInt64})");
             //var id = new PublishedFileId(2040656402ul);
             //PlatformService.workshop.RequestItemDetails(id).LogRet($"RequestItemDetails({id.AsUInt64})");
         }
-        public static void OnQueryItemsClicked()
-        {
+        public static void OnQueryItemsClicked() {
             Log.Debug("QueryItems pressed");
             PlatformService.workshop.QueryItems().LogRet($"QueryItems()"); ;
         }
 
-        private static void OnSubmitItemUpdate(SubmitItemUpdateResult result, bool ioError)
-        {
+        private static void OnSubmitItemUpdate(SubmitItemUpdateResult result, bool ioError) {
             Log.Debug($"PlatformService.workshop.eventSubmitItemUpdate(result:{result.result}, {ioError})");
         }
 
 
-        private static void OnGameOverlayActivated(bool active)
-        {
+        private static void OnGameOverlayActivated(bool active) {
             Log.Debug($"PlatformService.workshop.eventGameOverlayActivated({active})");
         }
 
-        private static void OnWorkshopItemInstalled(PublishedFileId id)
-        {
+        private static void OnWorkshopItemInstalled(PublishedFileId id) {
             Log.Debug($"PlatformService.workshop.eventWorkshopItemInstalled({id.AsUInt64})");
         }
 
-        private static void OnWorkshopSubscriptionChanged(PublishedFileId id, bool subscribed)
-        {
+        private static void OnWorkshopSubscriptionChanged(PublishedFileId id, bool subscribed) {
             Log.Debug($"PlatformService.workshop.eventWorkshopSubscriptionChanged({id.AsUInt64}, {subscribed})");
         }
 
-        static void OnUGCQueryCompleted(UGCDetails result, bool ioError)
-        {
+        static void OnUGCQueryCompleted(UGCDetails result, bool ioError) {
             // called after QueryItems
             Log.Debug($"OnUGCQueryCompleted(" +
                 $"result:{result.ToSTR()}, " +
                 $"ioError:{ioError})");
         }
 
-        static void OnUGCRequestUGCDetailsCompleted(UGCDetails result, bool ioError)
-        {
+        static void OnUGCRequestUGCDetailsCompleted(UGCDetails result, bool ioError) {
             // called after RequestItemDetails
             //Log.Debug($"OnUGCRequestUGCDetailsCompleted(" +
             //    $"result:{result.ToSTR2()}, " +
@@ -226,8 +207,7 @@ namespace LoadOrderInjections {
         public static string ToSTR(this ref UGCDetails result)
             => $"UGCDetails({result.result} {result.publishedFileId.AsUInt64})";
 
-        public static string ToSTR2(this ref UGCDetails result)
-        {
+        public static string ToSTR2(this ref UGCDetails result) {
 #pragma warning disable
             string m =
                 $"UGCDetails:\n" +
@@ -245,14 +225,13 @@ namespace LoadOrderInjections {
         }
 
         //code copied from package entry
-        public static DateTime GetLocalTimeCreated(string modPath)
-        {
+        public static DateTime GetLocalTimeCreated(string modPath) {
             DateTime dateTime = DateTime.MinValue;
 
             foreach (string path in Directory.GetFiles(modPath)) {
                 string ext = Path.GetExtension(path);
                 //if (ext == ".dll" || ext == ".crp" || ext == ".png")
-                { 
+                {
                     DateTime creationTimeUtc = File.GetCreationTimeUtc(path);
                     if (creationTimeUtc > dateTime) {
                         dateTime = creationTimeUtc;
@@ -263,8 +242,7 @@ namespace LoadOrderInjections {
         }
 
         //code copied from package entry
-        public static DateTime GetLocalTimeUpdated(string modPath)
-        {
+        public static DateTime GetLocalTimeUpdated(string modPath) {
             DateTime dateTime = DateTime.MinValue;
             if (Directory.Exists(modPath)) {
                 foreach (string path in Directory.GetFiles(modPath)) {
@@ -282,6 +260,8 @@ namespace LoadOrderInjections {
         }
 
         public static DateTime kEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        public static DateTime ToUTCTime(this uint time) => kEpoch.AddSeconds(time);
         public static DateTime ToLocalTime(this uint time) => kEpoch.AddSeconds(time).ToLocalTime();
 
         public static string ToAuthorName(this UserID userID) => new Friend(userID).personaName;
@@ -292,25 +272,37 @@ namespace LoadOrderInjections {
             NotDownloaded,
             PartiallyDownloaded,
         }
-        public static bool IsUGCUpToDate(UGCDetails data, out string reason)
-        {
-            string localPath = GetSubscribedItemFinalPath(data.publishedFileId);
+
+        static string STR(DateTime time) {
+            var local = time.ToLocalTime().ToString();
+            var utc = time.ToUniversalTime().ToShortTimeString();
+            return $"{local} (UTC {utc})";
+        }
+
+        public static bool IsUGCUpToDate(UGCDetails det, out string reason) {
+            string localPath = GetSubscribedItemFinalPath(det.publishedFileId);
             if (localPath == null) {
                 reason = "subscribed item is not downloaded. path does not exits: " +
-                    PlatformService.workshop.GetSubscribedItemPath(data.publishedFileId);
+                    PlatformService.workshop.GetSubscribedItemPath(det.publishedFileId);
                 return false;
             }
 
-            var updatedServer = data.timeUpdated.ToLocalTime();
-            var updatedLocal = GetLocalTimeUpdated(localPath);
-            if (updatedLocal < updatedServer) {
-                reason = $"subscribed item is out of date. server-time={updatedServer}  local-time={updatedLocal}";
-                return false;
-            }
-
-            var sizeServer = data.fileSize;
+            var updatedServer = det.timeUpdated.ToUTCTime();
+            var updatedLocal = GetLocalTimeUpdated(localPath).ToUniversalTime();
+            var sizeServer = det.fileSize;
             var localSize = GetTotalSize(localPath);
-            if (sizeServer > localSize) // could be smaller if user has its own files in there.
+            if (updatedLocal < updatedServer) {
+                bool serious =
+                    localSize < sizeServer ||
+                    updatedLocal < updatedServer.AddHours(-24);
+                string be = serious ? "is" : "may be";
+                reason = $"subscribed item {be} out of date.\n\t" +
+                    $"server-time={STR(updatedServer)} |  local-time={STR(updatedLocal)}";
+                return false;
+            }
+
+
+            if (localSize < sizeServer ) // could be smaller if user has its own files in there.
             {
                 reason = $"subscribed item download is incomplete. server-size={sizeServer}) local-size={localSize})";
                 return false;
@@ -320,14 +312,12 @@ namespace LoadOrderInjections {
             return true;
         }
 
-        public static long GetTotalSize(string path)
-        {
+        public static long GetTotalSize(string path) {
             var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
             return files.Sum(_f => new FileInfo(_f).Length);
         }
 
-        public static string GetSubscribedItemFinalPath(PublishedFileId id)
-        {
+        public static string GetSubscribedItemFinalPath(PublishedFileId id) {
             string path = PlatformService.workshop.GetSubscribedItemPath(id);
             if (!Directory.Exists(path)) {
                 path = ToExcludedPath(path);
@@ -336,11 +326,10 @@ namespace LoadOrderInjections {
             }
             return path;
         }
-        public static string ToExcludedPath(string includedPath)
-        {
+        public static string ToExcludedPath(string includedPath) {
             string p1 = Path.GetDirectoryName(includedPath);
             string p2 = Path.GetFileName(includedPath);
-            if(string.IsNullOrEmpty(p1) || string.IsNullOrEmpty(p2)) {
+            if (string.IsNullOrEmpty(p1) || string.IsNullOrEmpty(p2)) {
                 Log.Error("LoadOrderInjections.ToExcludedPath()\n" +
                     $"includedPath={includedPath}" +
                     $"p1={p1}\n" +
@@ -354,8 +343,7 @@ namespace LoadOrderInjections {
             return Path.Combine(p1, p2);
         }
 
-        public static void EnsureIncludedOrExcluded(PublishedFileId id)
-        {
+        public static void EnsureIncludedOrExcluded(PublishedFileId id) {
             try {
                 string path1 = PlatformService.workshop.GetSubscribedItemPath(id);
                 string path2 = ToExcludedPath(path1);
@@ -364,12 +352,11 @@ namespace LoadOrderInjections {
                     Directory.Move(path1, path2);
                 }
             } catch (Exception ex) {
-                Log.Exception(ex, $"EnsureIncludedOrExcluded({id})" ,showInPanel: false);
+                Log.Exception(ex, $"EnsureIncludedOrExcluded({id})", showInPanel: false);
             }
         }
 
-        public static void DeleteExtra()
-        {
+        public static void DeleteExtra() {
             Log.Info("DeleteExtra called ...");
             var items = PlatformService.workshop.GetSubscribedItems();
             if (items == null || items.Length == 0)
@@ -393,8 +380,7 @@ namespace LoadOrderInjections {
             }
         }
 
-        public static void EnsureAll()
-        {
+        public static void EnsureAll() {
             Log.Info("EnsureAll called ...");
             var items = PlatformService.workshop.GetSubscribedItems();
             foreach (var id in items) {
