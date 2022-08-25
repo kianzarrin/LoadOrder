7c97aa911d1656ef46422d7ba278c8713236de86
kianzzarrin <kian.zarrin@gmail.com>
2022-08-25 18:52:07 +0300

    future proofing: generic DLC name matching.

--- LoadOrderMod/Patches/ContentManager/IsDLCOwnedPatch.cs
+++ LoadOrderMod/Patches/ContentManager/IsDLCOwnedPatch.cs
@@ -10,34 +10,32 @@ namespace LoadOrderMod.Patches.ContentManager {
     [HarmonyPatch(typeof(SteamHelper), nameof(SteamHelper.IsDLCOwned))]
     public static class IsDLCOwnedPatch {
         static SteamHelper.DLC[] ExcludedDLCs;
+
+        static IEnumerable<SteamHelper.DLC> DLCsStartingWith(string name) {
+            foreach (SteamHelper.DLC dlc in Enum.GetValues(typeof(SteamHelper.DLC))) {
+                if (dlc.ToString().StartsWith(name, StringComparison.OrdinalIgnoreCase)) {
+                    yield return dlc;
+                }
+            }
+        }
+
         static void Prepare(MethodBase original) {
             Log.Called(original);
             if (ExcludedDLCs != null) return;
             var dlcs = new List<SteamHelper.DLC>();
             foreach(string item in ConfigUtil.Config.ExcludedDLCs) {
                 if (item == "MusicDLCs") {
-                    dlcs.Add(SteamHelper.DLC.RadioStation1);
-                    dlcs.Add(SteamHelper.DLC.RadioStation2);
-                    dlcs.Add(SteamHelper.DLC.RadioStation3);
-                    dlcs.Add(SteamHelper.DLC.RadioStation4);
-                    dlcs.Add(SteamHelper.DLC.RadioStation5);
-                    dlcs.Add(SteamHelper.DLC.RadioStation6);
-                    dlcs.Add(SteamHelper.DLC.RadioStation7);
-                    dlcs.Add(SteamHelper.DLC.RadioStation9);
-                    dlcs.Add(SteamHelper.DLC.RadioStation10);
-                    dlcs.Add(SteamHelper.DLC.RadioStation11);
+                    var radioDLCs = DLCsStartingWith("RadioStation");
+                    dlcs.AddRange(radioDLCs);
                 } else if (item == "Football") {
-                    dlcs.Add(SteamHelper.DLC.Football);
-                    dlcs.Add(SteamHelper.DLC.Football2);
-                    dlcs.Add(SteamHelper.DLC.Football3);
-                    dlcs.Add(SteamHelper.DLC.Football4);
-                    dlcs.Add(SteamHelper.DLC.Football5);
-                    dlcs.Add(SteamHelper.DLC.Football2345);
+                    var footballDLCs = DLCsStartingWith("Football");
+                    dlcs.AddRange(footballDLCs);
                 } else {
                     dlcs.Add((SteamHelper.DLC)Enum.Parse(typeof(SteamHelper.DLC), item));
                 }
             }
             ExcludedDLCs = dlcs.ToArray();
+            Log.Info($"ExcludedDLCs={ExcludedDLCs.ToSTR()}");
         }
 
 
